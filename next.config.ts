c69b2c5821c0399ae390adb212573f06fc383aa1
Frederick Socorin <frederick.socorin@xdnainteractive.com>
2025-07-02 16:23:00 +0800

    build

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 
+module.exports = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
 const nextConfig: NextConfig = {
   /* config options here */
 };
